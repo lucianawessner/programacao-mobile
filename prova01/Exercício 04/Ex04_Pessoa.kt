1738a8db942a5858e86d234f771b3c6744b07dea
Luciana Wesssner <lucianawessner@hotmail.com>
2024-09-27 12:59:07 -0300

    Adição Exercicio 4 - Pessoa

--- prova01/Exercício 04/Ex04_Pessoa.kt
+++ prova01/Exercício 04/Ex04_Pessoa.kt
@@ -1,1 +1,17 @@
+//Exercicio 04
+// Crie uma classe Pessoa com os atributos nome, idade e cidade. 
+//Implemente um método mostrarInformacoes que exiba todas as informações da pessoa. 
+//Crie um objeto Pessoa e chame o método mostrarInformacoes.
+
+// Nome Integrantes: Lucas Coelho e Luciana Wessner
+
+// Definindo a classe Pessoa
+class Pessoa(val nome: String, val idade: Int, val cidade: String) {
+    
+    // Método que exibe as informações da pessoa
+    fun mostrarInformacoes() {
+        // Imprime os atributos da pessoa formatados
+        println("Nome: $nome, Idade: $idade, Cidade: $cidade")
+    }
+}
 
