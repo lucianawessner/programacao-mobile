b72956052449aeb3e6d4c7621dd8294bd6d6891d
Luciana Wesssner <lucianawessner@hotmail.com>
2024-09-27 13:08:41 -0300

    Adição Exercício 5 - Animal

--- prova01/Exercício 05/Ex05_Animal.kt
+++ prova01/Exercício 05/Ex05_Animal.kt
@@ -1,1 +1,18 @@
+//Exercicio 05
+// Crie uma classe base Animal com os atributos nome e idade. 
+//Crie uma subclasse Cachorro que herde de Animal e adicione um atributo raca. 
+//Implemente um método latir na classe Cachorro. 
+//Crie um objeto Cachorro e chame os métodos latir e mostrarInformacoes (herdado de Animal).
 
+// Nome Integrantes: Lucas Coelho e Luciana Wessner
+
+
+// Classe base Animal, que pode ser estendida por outras classes
+open class Animal(val nome: String, val idade: Int) {
+    
+    // Método para mostrar as informações do animal
+    fun mostrarInformacoes() {
+        // Imprime o nome e a idade do animal
+        println("Nome: $nome, Idade: $idade")
+    }
+}
