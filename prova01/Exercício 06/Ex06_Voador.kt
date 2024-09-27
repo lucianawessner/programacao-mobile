b08ea1076fce2db3da3db2f32823a3e7d39d4128
Luciana Wesssner <lucianawessner@hotmail.com>
2024-09-27 13:15:29 -0300

    Adição Exercicio 6 - Voador

--- prova01/Exercício 06/Ex06_Voador.kt
+++ prova01/Exercício 06/Ex06_Voador.kt
@@ -1,1 +1,14 @@
+//Exercicio 06
+// Crie uma interface Voador com um método voar. 
+//Crie uma classe Passaro que implemente a interface Voador. 
+//Crie uma classe Helicoptero que implemente a interface Voador. 
+//Crie 3 objetos Passaro e dois objetosVoador, insira eles em uma lista e percorra a lista chamando o método voar.
+
+// Nome Integrantes: Lucas Coelho e Luciana Wessner
+
+// Definição da interface Voador
+interface Voador {
+    // Método que deve ser implementado por classes que usam esta interface
+    fun voar()
+}
 
