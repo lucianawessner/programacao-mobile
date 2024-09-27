3fc32bd4503867f337c7e5e46296b8e7dcdbb880
Luciana Wesssner <lucianawessner@hotmail.com>
2024-09-27 13:23:32 -0300

    Adição Exercicio 7 - Produto

--- prova01/Exercício 07/Ex07_Produto.kt
+++ prova01/Exercício 07/Ex07_Produto.kt
@@ -1,1 +1,12 @@
+//Exercicio 07
+// Crie uma classe Produto com os atributos nome, preco e quantidade. 
+//Crie uma lista com 10 objetos Produto. Ordene a lista de produtos por:
+//Preço em ordem crescente
+//Nome em ordem alfabética
+//Quantidade em ordem decrescente
 
+// Nome Integrantes: Lucas Coelho e Luciana Wessner
+
+
+// Definindo a classe Produto como uma data class
+data class Produto(val nome: String, val preco: Double, val quantidade: Int)
